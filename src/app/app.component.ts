ebb86bceb0cf661970946ae68bc1882d86b93051
Leo Velez <leonardo.velez@kaplan.edu>
2022-11-28 10:39:59 -0500

    Most functionality live

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('showButton', [
+      transition(':enter', [
+        style({opacity: 0, scale: 0}),
+        animate('.6s ease-in-out', style({opacity: 1, scale: 1}))
+      ]),
+      transition(':leave', [
+        animate('.6s ease-in-out', style({opacity: 0, scale: 0}))
+      ]),
+    ]),
+    trigger('toggleSidebar', [
+      transition(':enter', [
+        style({transform: 'translate3d(-100%, 0, 0'}),
+        animate('400ms ease-in-out', style({transform: 'translate3d(0,0,0)'})),
+      ]),
+      transition(':leave', [
+        animate('400ms ease-in-out', style({transform: 'translate3d(-200%, 0, 0)'}))
+      ]),
+    ]),
+  ]
 })
-export class AppComponent {
-  title = 'card-builder';
+export class AppComponent implements OnInit {
+  cardData = new FormGroup({
+    itemName: new FormControl(''),
+    flavor: new FormControl(''),
+    description: new FormControl(''),
+    keywords: new FormControl(''),
+  });
+
+  cardDeck: Array<any> = [];
+  clipBoard = '';
+  showSidebar = true;
+
+  ngOnInit() {
+    const lStore = window.localStorage.getItem('card_builder_deck');
+    if (lStore) {
+      this.cardDeck.push(...JSON.parse(lStore));
+    }
+  }
+
+  clearDeck() {
+    this.cardDeck = [];
+    window.localStorage.removeItem('card_builder_deck');
+  }
+
+  clearForm() {
+    this.cardData.reset();
+  }
+
+  copyCard(index: number) {
+    const cardData = this.cardDeck[index];
+    const title = `__**${cardData.itemName.toUpperCase()}**__\r\n`;
+    const flavor = `*${cardData.flavor}*\r\n`;
+    const descrip = `${cardData.description}\r\n`;
+    const keywords = `*${cardData.keywords.toLowerCase()}*`;
+    
+    this.clipBoard =
+      title + flavor + descrip + keywords;
+
+    navigator.clipboard.writeText(this.clipBoard);
+  }
+
+  onSubmit() {
+    this.cardDeck.push(this.cardData.value);
+    window.localStorage.setItem('card_builder_deck', JSON.stringify(this.cardDeck));
+
+    const lStore = window.localStorage.getItem('card_builder_deck');
+    console.log(lStore);
+  }
+
+  print() {
+    window.print();
+  }
+
+  remove(event: any) {
+    this.cardDeck.splice(event, 1);
+    window.localStorage.setItem('card_builder_deck', JSON.stringify(this.cardDeck));
+  }
 }
