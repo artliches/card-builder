b5522263c13c6ca5b1df04731673d998eb5f200c
Leo Velez <leonardo.velez@kaplan.edu>
2022-12-08 13:13:54 -0500

    UI Overhaul+download/upload decks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,6 +46,8 @@ import { FormControl, FormGroup } from '@angular/forms';
   ]
 })
 export class AppComponent implements OnInit {
+  @ViewChild('fileUploadInput', {static: false}) inputRef: any;
+
   cardData = new FormGroup({
     itemName: new FormControl(''),
     flavor: new FormControl(''),
@@ -56,6 +58,7 @@ export class AppComponent implements OnInit {
   cardDeck: Array<any> = [];
   clipBoard = '';
   showSidebar = true;
+  cardDeckJSON = '';
 
   ngOnInit() {
     const lStore = window.localStorage.getItem('card_builder_deck');
@@ -89,13 +92,37 @@ export class AppComponent implements OnInit {
   onSubmit() {
     this.cardDeck.push(this.cardData.value);
     window.localStorage.setItem('card_builder_deck', JSON.stringify(this.cardDeck));
-
-    const lStore = window.localStorage.getItem('card_builder_deck');
-    console.log(lStore);
   }
 
-  print() {
-    window.print();
+  download() {
+    const dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(this.cardDeck));
+    const dlAnchorElem = document.getElementById('downloadAnchorElem');
+
+    if (dlAnchorElem) {
+      dlAnchorElem.setAttribute('href', dataStr);
+      dlAnchorElem.setAttribute('download', `cardDeck.json`);
+    }
+  }
+
+  loadDeck(event: any) {
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+
+    fileReader.readAsText(selectedFile, 'UTF-8');
+    fileReader.onload = () => {
+      if (fileReader.result?.toString()) {
+        this.cardDeck = JSON.parse(fileReader.result.toString());
+        window.localStorage.setItem('card_builder_deck', JSON.stringify(this.cardDeck));
+        this.reset();
+      }
+    };
+    fileReader.onerror = (error) => {
+      console.log(error);
+    };
+  }
+
+  reset() {
+    this.inputRef.nativeElement.value = '';
   }
 
   remove(event: any) {
